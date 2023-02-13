cf6d5a8f4a7f6cbecc432684ee09bb6c8e91ecaa
Pham Tien Anh <phamtienanhdev@gmail.com>
2023-02-13 22:31:33 +0700

    Stack

---
+++ Array/Find_Subarray_with_given_sum/Solution.java
@@ -0,0 +1,20 @@
+package Array.Find_Subarray_with_given_sum;
+// Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+// Output: Sum found between indexes 2 and 4
+
+// Input: arr[] = {1, 4, 0, 0, 3, 10, 5}, sum = 7
+// Output: Sum found between indexes 1 and 4
+
+// Input: arr[] = {1, 4}, sum = 0
+// Output: No subarray found
+public class Solution {
+  public static void BruteForce(int[] arr, int sum){
+
+  }
+  public static void main(String[] args) {
+    int[] arr = {1, 4, 20, 3, 10, 5};
+    int sum = 33;
+    BruteForce(arr, sum);
+    
+  }
+}
---
+++ Stack/Convert_Infix_expression_to_Postfix_expression/Solution.java
@@ -0,0 +1,12 @@
+package Stack.Convert_Infix_expression_to_Postfix_expression;
+// Input: A + B * C + D
+// Output: ABC*+D+
+
+// Input: ((A + B) – C * (D / E)) + F
+// Output: AB+CDE/*-F+  
+public class Solution {
+  
+  public static void main(String[] args) {
+    
+  }
+}
